f12116ef9a5fd5932292af3ff8ef0ea4d922d64a
nemetviiktor <dajlefromcb@gmail.com>
2021-11-14 08:07:43 +0100

    formatting

--- src/main/java/com/gp/Generalpractitioner/service/DateUtil.java
+++ src/main/java/com/gp/Generalpractitioner/service/DateUtil.java
@@ -2,11 +2,9 @@ package com.gp.Generalpractitioner.service;
 
 import java.sql.Date;
 
-
 public class DateUtil {
-	
+
 	public Date getCurrentDate() {
 		return new Date(System.currentTimeMillis());
 	}
-	
 }
